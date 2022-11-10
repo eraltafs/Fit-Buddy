d3caa23f270cb1addd7b7e94800ef39fd3128760
AnkitChopde <acchopde84@gmail.com>
2022-11-10 11:55:03 +0530

    welcome

---
+++ components/login_nav.js
@@ -0,0 +1,13 @@
+
+
+function nav(){
+    return `
+    <div><img src="https://i.ibb.co/5BYTtSP/Fit-Buddies.png" alt=""></div>
+    <div>
+        <a href="login.html"><li>LOG IN </li></a><span>|</span>
+       <a href="./Signup_pages/welcome.html"> <li>SIGN UP</li></a>
+    </div>
+    `
+}
+
+export {nav}
--- login.js
+++ login.js
@@ -1,7 +1,14 @@
 
+import {nav} from "./components/login_nav.js"
+
+let nav_div = document.getElementById('nav');
+
+nav_div.innerHTML = nav();
+
 
 import footer from "./components/login_footer.js"
 
 let footer_div = document.getElementById('footer');
 
 footer_div.innerHTML = footer();
+
