d1bb0ab561f03fc0c28007a57f1dff8090533df0
eraltafs <teacheraltafs@e.com>
2022-11-11 06:42:40 +0530

    page linked

--- components/login_nav.js
+++ components/login_nav.js
@@ -2,9 +2,8 @@
 
 
 
-
 function loginNav(){
-    return `<div><img src="https://i.ibb.co/5BYTtSP/Fit-Buddies.png" alt=""></div>
+    return `<div><img onclick = "location.href = '../index.html'" src="https://i.ibb.co/5BYTtSP/Fit-Buddies.png" alt=""></div>
     <div>
         <a href="login.html"><li>LOG IN </li></a><span>|</span>
        <a href="./Signup_pages/welcome.html"> <li>SIGN UP</li></a>
