d68ecb43a9a7084b855964b2ba5ca61e6f1cb2d2
AnkitChopde <acchopde84@gmail.com>
2022-11-11 09:41:52 +0530

    day-4

--- Signup_pages/activity.js
+++
@@ -1,7 +0,0 @@
-
-
-import {loginNav} from "./components/login_nav.js"
-
-let login_div = document.getElementById('nav')
-
-login_div.innerHTML = loginNav();
--- components/login_nav.js
+++ components/login_nav.js
@@ -6,8 +6,8 @@
 function loginNav(){
     return `<div><img src="https://i.ibb.co/5BYTtSP/Fit-Buddies.png" alt=""></div>
     <div>
-        <a href="login.html"><li>LOG IN </li></a><span>|</span>
-       <a href="./Signup_pages/welcome.html"> <li>SIGN UP</li></a>
+        <a href="login.html"><li id="logIn">LOG IN </li></a><span id="line">|</span>
+       <a href="welcome.html"> <li id="signUp">SIGN UP</li></a>
     </div>`
 }
 
