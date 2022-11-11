5293d602453ff9c1e5fa532574a990cb705529fd
AnkitChopde <acchopde84@gmail.com>
2022-11-11 13:31:46 +0530

    welcome

--- scripts/goal.js
+++ scripts/goal.js
@@ -1,4 +1,4 @@
-import {loginNav} from "./components/login_nav.js"
+import {loginNav} from "../components/login_nav.js"
 
 let login_div = document.getElementById('nav')
 
--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,6 +1,6 @@
 
 
-import {loginNav} from "./components/login_nav.js"
+import {loginNav} from "../components/login_nav.js"
 
 let login_div = document.getElementById('nav')
 
