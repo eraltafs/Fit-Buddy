c035eec1148f221950de558f148c7181967230a4
AnkitChopde <acchopde84@gmail.com>
2022-11-11 22:14:14 +0530

    Day-4

--- scripts/weight_page.js
+++ scripts/weight_page.js
@@ -18,33 +18,54 @@ log1.style.display = 'none';
 let b = document.getElementById('btn1');
 let b1 = document.getElementById('btn2');
 let b2 = document.getElementById('btn3');
+let p = document.getElementById('display');
+let count=0;
 b.onclick=()=>{
+    p.innerHTML = null;
      b.style.border="2px solid #0066EE"
      b.style.color="#0066EE "
      b1.style.border="1px solid #111010b9"
      b1.style.color="#111010b9"
      b2.style.border="1px solid #111010b9"
      b2.style.color="#111010b9"
-
+     count++;
 }
 b1.onclick=()=>{
+    p.innerHTML = null;
     b1.style.border="2px solid #0066EE"
     b1.style.color="#0066EE"
     b.style.border="1px solid #111010b9"
     b.style.color="#111010b9"
     b2.style.border="1px solid #111010b9"
     b2.style.color="#111010b9"
-
+    count++;
 }
 b2.onclick=()=>{
+    p.innerHTML = null;
     b2.style.border="2px solid #0066EE"
     b2.style.color="#0066EE"
     b.style.border="1px solid #111010b9"
     b.style.color="#111010b9"
     b1.style.border="1px solid #111010b9"
     b1.style.color="#111010b9"
-
+    count++;
 }
+
+let next = document.getElementById('next');
+
+    next.onclick=()=>{
+        if(count>0){
+            replace();
+        }else{
+            
+            p.innerHTML = "Please select a goal."
+            p.style.color="red";
+        }
+    }
+function replace(){
+    window.location.href="activity.html"
+}
+ 
 
 
 
