964d6e30e9a17c6469e6d6f32fd4367da42b9b19
eraltafs <43698833+eraltafs@users.noreply.github.com>
2022-11-12 15:47:50 +0530

    slider added

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,8 @@
 document.getElementById("prem").onclick = () => {
   location.href = "./premium.html";
 };
-function slideshow() {
-  let images = [
-    `https://www.myfitnesspal.com/_next/image?url=%2Fpages%2Fhome%2Fvictory-story-1.jpg&w=1920&q=75`,
-    `https://www.myfitnesspal.com/_next/image?url=%2Fpages%2Fhome%2Fvictory-story-2.jpg&w=1920&q=75`,
-    `https://www.myfitnesspal.com/_next/image?url=%2Fpages%2Fhome%2Fvictory-story-3.jpg&w=1920&q=75`,
-  ];
 
-  let imgelement = document.createElement("img");
-  slideshow_div.append(imgelement);
-  let i = 1;
-  setInterval(function () {
-    if (i == images.length) {
-      i = 0;
-    }
-    imgelement.src = images[i];
-    slideshow_div.append(imgelement);
-    i++;
-  }, 2000);
-}
-slideshow();
+
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
 const swiper = new Swiper(".swiper", {
