e9e8f4cac5756e08b240680ee248413fe9d349a0
Ankesh Chopde <acchopde84@gmail.com>
2023-05-29 11:45:50 +0530

    responsive done

--- components/login_nav.js
+++ components/login_nav.js
@@ -4,7 +4,7 @@
 
 function loginNav(){
     return `<div><img onclick ="location.href = './index.html'" src="https://i.ibb.co/Y8k72m4/Fit-Buddies-1.png" alt=""></div>
-    <div>
+    <div id="navbar">
         <a href="login.html"><li id="logIn">LOG IN </li></a><span id="line">|</span>
        <a href="welcome.html"> <li id="signUp">SIGN UP</li></a>
     </div>`
