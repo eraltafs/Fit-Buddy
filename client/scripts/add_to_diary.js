41e9dd551e8c22332a4293c0a2f4a1d896c929e5
eraltafs <43698833+eraltafs@users.noreply.github.com>
2023-05-30 22:23:26 +0530

    bug fixed

--- client/scripts/add_to_diary.js
+++ client/scripts/add_to_diary.js
@@ -1,20 +1,19 @@
-
-  document.getElementById("navexeciseanch").style.backgroundColor = "#00548b";
-
-
+document.getElementById("navexeciseanch").style.backgroundColor = "#00548b";
 
 let userDetails = document.getElementById("username");
 userDetails.innerText = null;
 
 let username = localStorage.getItem("user");
 if (username === undefined) {
-	window.location = "login.html";
+  window.location = "login.html";
 }
 userDetails.innerText = username;
 
 document.getElementById("searchexerbtn").onclick = async () => {
   let search = document.getElementById("searchexerinp").value;
-  let res = await fetch(`https://server-fitbuddy.onrender.com/exercise?title=${search}`);
+  let res = await fetch(
+    `https://server-fitbuddy.onrender.com/exercise?title=${search}`
+  );
   data = await res.json();
   Append(data);
 };
@@ -37,26 +36,33 @@ function Append(data) {
       var minute;
       var calories;
       duration.oninput = () => {
-        minute = duration.value
+        minute = duration.value;
         let h = Math.floor(minute * 9.2167);
         document.getElementById("calories").value = h;
         calories = document.getElementById("calories").value;
       };
       document.getElementById("AddExercise").onclick = async () => {
-        let send = {title,
-            calories,
-            minute,
-        } 
-        let res = await fetch(`https://server-fitbuddy.onrender.com/user/6474d03465e890eecd95cd89/excercise`, {
-          method: "POST",
-          body: JSON.stringify(send),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        console.log(await res.json())
-        document.getElementById('entry').style.display="block";
+        let send = { title, calories, minute };
+        let res = await fetch(
+          `https://server-fitbuddy.onrender.com/user/6474d03465e890eecd95cd89/excercise`,
+          {
+            method: "POST",
+            body: JSON.stringify(send),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        let jsonData = await res.json();
+        if (jsonData.msg === "item added to menu"){
+          alert("data updated")
+          location.reload()
+        }
+        else {
+          alert("something went wrong")
 
+        }
+        document.getElementById("entry").style.display = "block";
       };
     };
     container.append(p);
