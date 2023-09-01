f1750ce49822261ef005113ee8b90f52fb6f155f
eraltafs <43698833+eraltafs@users.noreply.github.com>
2023-09-01 23:38:29 +0530

    UI updated

--- client/scripts/exercise.js
+++ client/scripts/exercise.js
@@ -31,6 +31,7 @@ if (!token) {
     let jsondata = await getprofile();
     const { email } = jsondata;
     userDetails.innerText = email;
+    fetchExercises();
   };
 }
 const hamburgerIcon = document.getElementById("hamburger-icon");
@@ -149,7 +150,7 @@ function Append(data) {
     no_data_message.style.color = "red";
   } else {
     no_data_message.style.display = "none";
-    data.forEach(({ _id, calories, title, minute },i) => {
+    data.forEach(({ _id, calories, title, minute }, i) => {
       let tr = document.createElement("tr");
       let td1 = document.createElement("td");
       td1.textContent = title;
@@ -163,7 +164,7 @@ function Append(data) {
       td4.style.color = "red";
       td4.style.cursor = "pointer";
       td4.onclick = async () => {
-        console.log(_id,i);
+        console.log(_id, i);
         let res = await fetch(
           `https://server-fitbuddy.onrender.com/user/excercise/${_id}`,
           {
@@ -175,14 +176,116 @@ function Append(data) {
           }
         );
         if (res.ok) {
-          alert("Exercise deleted")
-          data.splice(i,1)
-          Append(data)
-        //   // window.location.reload();
+          alert("Exercise deleted");
+          data.splice(i, 1);
+          Append(data);
+          //   // window.location.reload();
         }
       };
       tr.append(td1, td2, td3, td4);
       body.append(tr);
     });
   }
+}
+
+const addButton = document.getElementById("addExercise");
+const popupDiv = document.getElementById("datafetcher");
+const closeButton = document.getElementById("closeButton");
+let selectExercise = document.getElementById("selectExercise");
+
+function showPopup() {
+  popupDiv.style.display = "block";
+}
+
+// Function to hide the popup
+function hidePopup() {
+  popupDiv.style.display = "none";
+}
+
+// Show the popup when the button is clicked
+addButton.addEventListener("click", showPopup);
+
+// Hide the popup when the close button is clicked
+closeButton.addEventListener("click", function (event) {
+  hidePopup();
+  // Prevent the click event from propagating to the document click event listener
+  event.stopPropagation();
+});
+
+// Hide the popup when clicked outside of it or anywhere in the body
+document.addEventListener("click", function (event) {
+  if (!popupDiv.contains(event.target) && event.target !== addButton) {
+    hidePopup();
+  }
+});
+
+const fetchExercises = async () => {
+  let res = await fetch(`https://server-fitbuddy.onrender.com/exercise`, {
+    headers: {
+      authentication: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+  let data = await res.json();
+  Appendoptions(data);
+};
+
+function Appendoptions(data) {
+  selectExercise.innerHTML = null;
+  var durationInput = document.getElementById("duration");
+  var caloriesInput = document.getElementById("calories");
+  var calories, minute, selectedExercise;
+  selectedExercise = data[0].title;
+  data.forEach(({ title }) => {
+    let opt = document.createElement("option");
+    opt.value = title;
+    opt.textContent = title;
+
+    selectExercise.onchange = () => {
+      selectedExercise = selectExercise.value;
+      console.log(selectedExercise);
+    };
+
+    durationInput.oninput = () => {
+      minute = durationInput.value;
+      let calCalories = Math.floor(minute * 9.2167);
+      caloriesInput.value = calCalories;
+      calories = caloriesInput.value;
+    };
+    caloriesInput.oninput = () => {
+      calories = caloriesInput.value;
+    };
+    document.getElementById("AddExercise").onclick = async () => {
+      let send = { title: selectedExercise, calories, minute };
+      console.log(send);
+      if (title && calories && minute) {
+        try {
+          let res = await fetch(
+            `https://server-fitbuddy.onrender.com/user/excercise`,
+            {
+              method: "POST",
+              body: JSON.stringify(send),
+              headers: {
+                authentication: `Bearer ${token}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          let jsonData = await res.json();
+          if (jsonData.msg === "item added to menu") {
+            alert("data updated");
+            location.reload();
+          } else {
+            alert("something went wrong");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        alert("please input all value");
+      }
+    };
+
+    selectExercise.append(opt);
+  });
 }
