727255fcb1e5fce964254d275b0028327e248141
eraltafs <43698833+eraltafs@users.noreply.github.com>
2023-09-08 12:32:56 +0530

    more functinality added

--- client/scripts/exercise.js
+++ client/scripts/exercise.js
@@ -6,8 +6,11 @@ let nav = document.querySelector("nav");
 let footer_sec = document.querySelector("footer");
 let decreament = document.querySelector(".fa-caret-left");
 let increament = document.querySelector(".fa-caret-right");
-let body = document.getElementById("exercisebody");
-let no_data_message = document.getElementById("message");
+let CardioBody = document.getElementById("CardioBody");
+let StrengthBody = document.getElementById("StrengthBody");
+
+let no_data_cardio = document.getElementById("messageCardio");
+let no_data_strength = document.getElementById("messageStrength");
 
 nav.innerHTML = navbar();
 footer_sec.innerHTML = footer();
@@ -31,7 +34,8 @@ if (!token) {
     let jsondata = await getprofile();
     const { email } = jsondata;
     userDetails.innerText = email;
-    fetchExercises();
+    fetchingCardio();
+    fetchingStrength();
   };
 }
 const hamburgerIcon = document.getElementById("hamburger-icon");
@@ -115,12 +119,13 @@ function updateDate() {
   monthn = monthNames[today.getMonth()];
   date = `${dayn}, ${monthn} ${dd}, ${yyyy}`;
   document.getElementById("date").textContent = date;
-  fetching();
+  fetchingCardio();
+  fetchingStrength();
 }
 
-async function fetching() {
+async function fetchingCardio() {
   let res = await fetch(
-    `https://server-fitbuddy.onrender.com/user/excercise?date=${yyyy}-${mm}-${dd}`,
+    `https://server-fitbuddy.onrender.com/user/cardio?date=${yyyy}-${mm}-${dd}`,
     {
       headers: {
         authentication: `Bearer ${token}`,
@@ -129,7 +134,7 @@ async function fetching() {
     }
   );
   let data = await res.json();
-  Append(data);
+  AppendCardio(data);
   let totalminute = 0;
   let totalcalories = 0;
   data.forEach(({ calories, minute }) => {
@@ -141,15 +146,29 @@ async function fetching() {
   document.getElementById("totcalories").textContent =
     `${totalcalories}` || "0";
 }
-fetching();
-function Append(data) {
-  body.innerHTML = null;
+
+async function fetchingStrength() {
+  let res = await fetch(
+    `https://server-fitbuddy.onrender.com/user/strength?date=${yyyy}-${mm}-${dd}`,
+    {
+      headers: {
+        authentication: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  let data = await res.json();
+  AppendStrength(data);
+}
+
+function AppendCardio(data) {
+  CardioBody.innerHTML = null;
   if (data.length === 0) {
-    no_data_message.style.display = "block";
-    no_data_message.textContent = `You have no data for ${date}`;
-    no_data_message.style.color = "red";
+    no_data_cardio.style.display = "block";
+    no_data_cardio.textContent = `You have no cardio excercise for ${date}`;
+    no_data_cardio.style.color = "red";
   } else {
-    no_data_message.style.display = "none";
+    no_data_cardio.style.display = "none";
     data.forEach(({ _id, calories, title, minute }, i) => {
       let tr = document.createElement("tr");
       let td1 = document.createElement("td");
@@ -164,9 +183,8 @@ function Append(data) {
       td4.style.color = "red";
       td4.style.cursor = "pointer";
       td4.onclick = async () => {
-        console.log(_id, i);
         let res = await fetch(
-          `https://server-fitbuddy.onrender.com/user/excercise/${_id}`,
+          `https://server-fitbuddy.onrender.com/user/cardio/${_id}`,
           {
             method: "DELETE",
             headers: {
@@ -178,72 +196,181 @@ function Append(data) {
         if (res.ok) {
           alert("Exercise deleted");
           data.splice(i, 1);
-          Append(data);
+          AppendCardio(data);
           //   // window.location.reload();
         }
       };
       tr.append(td1, td2, td3, td4);
-      body.append(tr);
+      CardioBody.append(tr);
     });
   }
 }
 
-const addButton = document.getElementById("addExercise");
-const popupDiv = document.getElementById("datafetcher");
-const closeButton = document.getElementById("closeButton");
-let selectExercise = document.getElementById("selectExercise");
+function AppendStrength(data) {
+  console.log(data);
+  StrengthBody.innerHTML = null;
+  if (data.length === 0) {
+    no_data_strength.style.display = "block";
+    no_data_strength.textContent = `You have no strength excercise for ${date}`;
+    no_data_strength.style.color = "red";
+  } else {
+    no_data_strength.style.display = "none";
+    data.forEach(({ _id, weight, title, sets }, i) => {
+      let tr = document.createElement("tr");
+      let td1 = document.createElement("td");
+      td1.textContent = title;
+      let td2 = document.createElement("td");
+      td2.textContent = sets;
+      let td3 = document.createElement("td");
+      td3.textContent = weight;
 
-function showPopup() {
-  popupDiv.style.display = "block";
+      let td4 = document.createElement("td");
+      td4.innerHTML = `<i class="fa-solid fa-circle-minus"></i>`;
+      td4.style.color = "red";
+      td4.style.cursor = "pointer";
+      td4.onclick = async () => {
+        let res = await fetch(
+          `https://server-fitbuddy.onrender.com/user/strength/${_id}`,
+          {
+            method: "DELETE",
+            headers: {
+              authentication: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (res.ok) {
+          alert("Exercise deleted");
+          data.splice(i, 1);
+          AppendStrength(data);
+          //   // window.location.reload();
+        }
+      };
+      tr.append(td1, td2, td3, td4);
+      StrengthBody.append(tr);
+    });
+  }
+}
+
+const addCardio = document.getElementById("addCardio");
+const addStrength = document.getElementById("addStrength");
+
+const cardioAdder = document.getElementById("cardioAdder");
+const strenthAdder = document.getElementById("strenthAdder");
+
+const closeButtonCardio = document.getElementById("closeButtonCardio");
+const closeButtonStrength = document.getElementById("closeButtonStrength");
+
+let selectCardio = document.getElementById("selectCardio");
+let selectSrength = document.getElementById("selectSrength");
+
+function showCadio(data) {
+  cardioAdder.style.display = "block";
+  AppendCardioOptions(data);
+}
+function showStrength(data) {
+  strenthAdder.style.display = "block";
+  AppendStrengthOptions(data);
 }
 
 // Function to hide the popup
-function hidePopup() {
-  popupDiv.style.display = "none";
+function hidePopup(element) {
+  element.style.display = "none";
 }
 
-// Show the popup when the button is clicked
-addButton.addEventListener("click", showPopup);
+addCardio.addEventListener("click", () => {
+  let cardioArr = [
+    "Running",
+    "Cycling",
+    "Swimming",
+    "Jumping Rope",
+    "Brisk Walking",
+    "Aerobics",
+    "Rowing",
+    "Elliptical Trainer",
+    "Stair Climbing",
+    "Boxing",
+    "Hiking",
+    "Dancing",
+    "CrossFit",
+    "Kickboxing",
+    "Circuit Training",
+    "Spinning",
+    "Soccer",
+    "Jumping Jacks",
+    "Burpees",
+  ];
+  showCadio(cardioArr);
+});
 
+addStrength.addEventListener("click", () => {
+  let strengthArr = [
+    "Push-Ups",
+    "Pull-Ups/Chin-Ups",
+    "Bench Press",
+    "Dumbbell Rows",
+    "Overhead Press",
+    "Bicep Curls",
+    "Tricep Dips",
+    "Lateral Raises",
+    "Squats",
+    "Deadlifts",
+    "Lunges",
+    "Leg Press",
+    "Calf Raises",
+    "Planks",
+    "Russian Twists",
+    "Hanging Leg Raises",
+    "Crunches",
+    "Reverse Crunches",
+    "Barbell Rows",
+    "Dumbbell Lunges",
+    "Push Press",
+    "Hamstring Curls",
+    "Seated Leg Extensions",
+    "Hammer Curls",
+    "Skull Crushers",
+    "Kettlebell Swings",
+    "Medicine Ball Slams",
+    "Bodyweight Squats",
+    "Turkish Get-Ups",
+    "Resistance Band Exercises",
+  ];
+  showStrength(strengthArr);
+});
 // Hide the popup when the close button is clicked
-closeButton.addEventListener("click", function (event) {
-  hidePopup();
-  // Prevent the click event from propagating to the document click event listener
-  event.stopPropagation();
+closeButtonCardio.addEventListener("click", function (event) {
+  hidePopup(cardioAdder);
+});
+
+closeButtonStrength.addEventListener("click", function (event) {
+  hidePopup(strenthAdder);
 });
 
 // Hide the popup when clicked outside of it or anywhere in the body
 document.addEventListener("click", function (event) {
-  if (!popupDiv.contains(event.target) && event.target !== addButton) {
-    hidePopup();
+  if (!cardioAdder.contains(event.target) && event.target !== addCardio) {
+    hidePopup(cardioAdder);
+  }
+  if (!strenthAdder.contains(event.target) && event.target !== addStrength) {
+    hidePopup(strenthAdder);
   }
 });
 
-const fetchExercises = async () => {
-  let res = await fetch(`https://server-fitbuddy.onrender.com/exercise`, {
-    headers: {
-      authentication: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-  });
-  let data = await res.json();
-  Appendoptions(data);
-};
-
-function Appendoptions(data) {
-  selectExercise.innerHTML = null;
+function AppendCardioOptions(data) {
+  selectCardio.innerHTML = null;
   var durationInput = document.getElementById("duration");
   var caloriesInput = document.getElementById("calories");
   var calories, minute, selectedExercise;
-  selectedExercise = data[0].title;
-  data.forEach(({ title }) => {
-    let opt = document.createElement("option");
-    opt.value = title;
-    opt.textContent = title;
+  selectedExercise = data[0];
 
-    selectExercise.onchange = () => {
-      selectedExercise = selectExercise.value;
-      console.log(selectedExercise);
+  data.forEach((i) => {
+    let opt = document.createElement("option");
+    opt.value = i;
+    opt.textContent = i;
+
+    selectCardio.onchange = () => {
+      selectedExercise = selectCardio.value;
     };
 
     durationInput.oninput = () => {
@@ -256,12 +383,13 @@ function Appendoptions(data) {
       calories = caloriesInput.value;
     };
     document.getElementById("AddExercise").onclick = async () => {
-      let send = { title: selectedExercise, calories, minute };
-      console.log(send);
+      let title = selectedExercise;
+      let send = { title, calories, minute };
+      console.log(send)
       if (title && calories && minute) {
         try {
           let res = await fetch(
-            `https://server-fitbuddy.onrender.com/user/excercise`,
+            `https://server-fitbuddy.onrender.com/user/cardio`,
             {
               method: "POST",
               body: JSON.stringify(send),
@@ -274,7 +402,7 @@ function Appendoptions(data) {
           let jsonData = await res.json();
           if (jsonData.msg === "item added to menu") {
             alert("data updated");
-            location.reload();
+            fetchingCardio();
           } else {
             alert("something went wrong");
           }
@@ -286,6 +414,63 @@ function Appendoptions(data) {
       }
     };
 
-    selectExercise.append(opt);
+    selectCardio.append(opt);
+  });
+}
+
+function AppendStrengthOptions(data) {
+  selectSrength.innerHTML = null;
+  var setsInput = document.getElementById("sets");
+  var weightInput = document.getElementById("weight");
+  var sets,weight, selectedStrength = data[0];
+
+  data.forEach((i) => {
+    let opt = document.createElement("option");
+    opt.value = i;
+    opt.textContent = i;
+
+    selectSrength.onchange = () => {
+      selectedStrength = selectSrength.value;
+    };
+
+    setsInput.oninput = () => {
+      sets = setsInput.value;
+    };
+    weightInput.oninput = () => {
+      weight = weightInput.value;
+    };
+    document.getElementById("AddStrength").onclick = async () => {
+      let title = selectedStrength;
+      let send = { title, sets, weight };
+      console.log(send)
+      if (title && sets && weight) {
+        try {
+          let res = await fetch(
+            `https://server-fitbuddy.onrender.com/user/strength`,
+            {
+              method: "POST",
+              body: JSON.stringify(send),
+              headers: {
+                authentication: `Bearer ${token}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          let jsonData = await res.json();
+          if (jsonData.msg === "item added to menu") {
+            alert("data updated");
+            fetchingStrength();
+          } else {
+            alert("something went wrong");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        alert("please input all value");
+      }
+    };
+
+    selectSrength.append(opt);
   });
 }
